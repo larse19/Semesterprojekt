50eaaf95b9ac575915ee84a7bc4cd97a2edd757f
larse19 <anders.l.h.larsen@live.dk>
2020-04-03 13:49:23 +0200

    Added Main class and cleanup

--- CredIT/src/main/java/org/example/App.java
+++ CredIT/src/main/java/org/example/App.java
@@ -31,7 +31,7 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    public static void main(String[] args) {
+    public static void launch(String[] args) {
         launch();
     }
 
