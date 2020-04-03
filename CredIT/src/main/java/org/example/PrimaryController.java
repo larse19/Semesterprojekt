b7b8c0327ff5b85ed1b6492c10c169ca181aa77f
larse19 <anders.l.h.larsen@live.dk>
2020-04-03 14:12:08 +0200

    Added Main class and cleanup

---
+++ CredIT/src/main/java/org/example/PrimaryController.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PrimaryController implements Initializable {
+
+    @FXML
+    private Button primaryButton;
+
+    @FXML
+    private Label textLabel;
+
+    @FXML
+    void switchToSecondary(ActionEvent event) {
+        textLabel.setText("Anders er for vild");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
