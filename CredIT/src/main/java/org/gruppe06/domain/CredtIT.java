c32e98dbcda3d6d9db0c2a3a57d95f699f3b4803
larse19 <anders.l.h.larsen@live.dk>
2020-04-17 16:22:36 +0200

    ProgramSystem implementation

--- CredIT/src/main/java/org/gruppe06/domain/CredtIT.java
+++ CredIT/src/main/java/org/gruppe06/domain/CredtIT.java
@@ -1,27 +1,7 @@
 package org.gruppe06.domain;
 
-import org.gruppe06.interfaces.IProgram;
-import org.gruppe06.persistance.DatabaseConnection;
-import org.gruppe06.persistance.ProgramDataHandler;
-
-import java.util.ArrayList;
 
 public class CredtIT {
-
-    private DatabaseConnection databaseConnection;
-    private static ProgramDataHandler programDataHandler;
-
-    public CredtIT(){
-        databaseConnection = DatabaseConnection.getDatabaseConnection();
-        programDataHandler = new ProgramDataHandler();
-    }
-
-    public static IProgram getProgram(String programName){
-        return programDataHandler.getProgram(programName);
-    }
-
-
-
 
 
 }
--- CredIT/src/main/java/org/gruppe06/presentation/SearchController.java
+++ CredIT/src/main/java/org/gruppe06/presentation/SearchController.java
@@ -2,14 +2,18 @@ package org.gruppe06.presentation;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import org.gruppe06.domain.CredtIT;
+import org.gruppe06.domain.ProgramSystem;
 import org.gruppe06.interfaces.ICastMember;
 import org.gruppe06.interfaces.IProducer;
 import org.gruppe06.interfaces.IProgram;
 
-public class SearchController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SearchController implements Initializable {
 
     @FXML
     private TextField searchTextField;
@@ -17,24 +21,37 @@ public class SearchController {
     @FXML
     private TextArea resultTextArea;
 
+    private ProgramSystem programSystem;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        programSystem = new ProgramSystem();
+    }
+
     @FXML
     void searchHandler(ActionEvent event) {
-        IProgram program = CredtIT.getProgram(searchTextField.getText());
-        String name = program.getName();
-        StringBuilder producers = new StringBuilder();
-        StringBuilder castMembers = new StringBuilder();
+        try {
+            IProgram program = programSystem.getProgram(searchTextField.getText());
+            String name = program.getName();
+            StringBuilder producers = new StringBuilder();
+            StringBuilder castMembers = new StringBuilder();
 
-        for(IProducer producer : program.getProducers()){
-            producers.append(producer.getName()).append(" \n");
+            for(IProducer producer : program.getProducers()){
+                producers.append(producer.getName()).append(" \n");
+            }
+
+            for(ICastMember castMember : program.getCast()){
+                castMembers.append(castMember.toString()).append("\n");
+            }
+
+            resultTextArea.setText("Title:\n" + name + "\n\n");
+            resultTextArea.appendText("Producers:\n" + producers + "\n");
+            resultTextArea.appendText("Cast:\n" + castMembers);
+        }catch (NullPointerException e){
+            resultTextArea.setText("Program not found");
         }
 
-        for(ICastMember castMember : program.getCast()){
-            castMembers.append(castMember.toString()).append("\n");
-        }
-
-        resultTextArea.setText("Title:\n" + name + "\n\n");
-        resultTextArea.appendText("Producers:\n" + producers + "\n");
-        resultTextArea.appendText("Cast:\n" + castMembers);
     }
+
 
 }
