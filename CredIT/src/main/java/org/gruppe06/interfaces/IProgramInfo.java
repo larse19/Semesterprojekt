711421d039fc291b490a28cb5d6eefd6566f1209
larse19 <anders.l.h.larsen@live.dk>
2020-05-08 14:11:26 +0200

    IProgramInfo implementation ProgramInfo moved to persistance

---
+++ CredIT/src/main/java/org/gruppe06/interfaces/IProgramInfo.java
@@ -0,0 +1,14 @@
+package org.gruppe06.interfaces;
+
+public interface IProgramInfo {
+
+    int getID();
+
+    String getName();
+
+    String getYear();
+
+    @Override
+    String toString();
+
+}
